ddb44c9df4b10541a60c97cf13a1d8e48b9d6de1
seannns <seannewmanshielsbusiness@gmail.com>
2025-03-05 15:17:07 +1000

    test macos commit

--- tutorial1/obj/Debug/net9.0/tutorial1.AssemblyInfo.cs
+++ tutorial1/obj/Debug/net9.0/tutorial1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tutorial1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08db65dc7236a93b245405a5e9e42df9cb1fa1bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("tutorial1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tutorial1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
